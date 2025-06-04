ddfbbcd42894c0f624b97001e8b91fe608717d1f
nitinguptamca <nitinguptamca@gmail.com>
2025-06-04 17:14:52 +0530

    initial commit

---
+++ src/test/java/com/nkg/prectise/kafka/reactive/ProducerServiceIntegrationTes.java
@@ -0,0 +1,72 @@
+package com.nkg.prectise.kafka.reactive;
+
+package com.example.integrationtestspringkafka.service;
+
+import com.example.integrationtestspringkafka.dto.ExampleDTO;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(topics = {"TOPIC_EXAMPLE", "TOPIC_EXAMPLE_EXTERNE"})
+public class ProducerServiceIntegrationTest {
+    private static final String TOPIC_EXAMPLE_EXTERNE = "TOPIC_EXAMPLE_EXTERNE";
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private ProducerService producerService;
+
+    public ExampleDTO mockExampleDTO(String name, String description) {
+        ExampleDTO exampleDTO = new ExampleDTO();
+        exampleDTO.setDescription(description);
+        exampleDTO.setName(name);
+        return exampleDTO;
+    }
+
+    /**
+     * We verify the output in the topic. With an simulated consumer.
+     */
+    @Test
+    public void itShould_ProduceCorrectExampleDTO_to_TOPIC_EXAMPLE_EXTERNE() {
+        // GIVEN
+        ExampleDTO exampleDTO = mockExampleDTO("Un nom", "Une description");
+        // simulation consumer
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("group_consumer_test", "false", embeddedKafkaBroker);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        ConsumerFactory cf = new DefaultKafkaConsumerFactory<String, ExampleDTO>(consumerProps, new StringDeserializer(), new JsonDeserializer<>(ExampleDTO.class, false));
+        Consumer<String, ExampleDTO> consumerServiceTest = cf.createConsumer();
+
+        embeddedKafkaBroker.consumeFromAnEmbeddedTopic(consumerServiceTest, TOPIC_EXAMPLE_EXTERNE);
+        // WHEN
+        producerService.send(exampleDTO);
+        // THEN
+        ConsumerRecord<String, ExampleDTO> consumerRecordOfExampleDTO = KafkaTestUtils.getSingleRecord(consumerServiceTest, TOPIC_EXAMPLE_EXTERNE);
+        ExampleDTO valueReceived = consumerRecordOfExampleDTO.value();
+
+        assertEquals("Une description", valueReceived.getDescription());
+        assertEquals("Un nom", valueReceived.getName());
+
+        consumerServiceTest.close();
+    }
+}
